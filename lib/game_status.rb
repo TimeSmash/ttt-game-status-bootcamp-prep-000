a9f2c9fc1f1a36a163b3695ad983615a7d6de8a3
mcumm64 <mcumm64@gmail.com>
2019-04-07 22:29:52 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -60,7 +60,7 @@ def draw? (board)
 end
 
 def over?(board)
-  if ((won?(board) && full?(board)) || ((won?(board) && !full?(board)) || draw?(board)
+  if (won?(board) && full?(board)) || ((won?(board) && !full?(board)) || draw?(board)
   true
   else
   false
